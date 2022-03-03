3169fed37fcfbcd854a129a24ebdd2359d960ae4
agaitskellwork <100278937+agaitskellwork@users.noreply.github.com>
2022-03-03 10:51:07 +0000

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,6 @@
-from flask import Flask  # Import the Flask class
-app = Flask(__name__)    # Create an instance of the class for our use
+#from flask import Flask  # Import the Flask class
+#app = Flask(__name__)    # Create an instance of the class for our use
+
+import flask
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
