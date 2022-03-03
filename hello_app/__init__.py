f8701cbd20bac3bac1f56e6e606067a38a142316
agaitskellwork <100278937+agaitskellwork@users.noreply.github.com>
2022-03-03 14:37:45 +0000

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -8,7 +8,7 @@ from dash import dcc
 
 from flask import render_template
 
-server = flask.Flask(__name__)
+server = flask.Flask(__name__, template_folder="templates")
 app = dash.Dash(__name__, server=server,  url_base_pathname='/dash/')
 
 #app.layout = html.Div(id='dash-container')
