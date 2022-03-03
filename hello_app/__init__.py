1158b6c9d84021864eb26a06ef94986331becd19
agaitskellwork <100278937+agaitskellwork@users.noreply.github.com>
2022-03-03 14:14:32 +0000

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -18,14 +18,68 @@ app.layout = html.Div([html.H1('Simple Layout',style={'textAlign':'center'})])
 def hello():
     return 'Hi, World!'
 
-@server.route('/home')
-def home():
-    return render_template('home.html')
-
 @server.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return f'User {escape(username)}'
+
+from dash import Input, Output, callback
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+index_page = html.Div([
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/page-2'),
+])
+
+page_1_layout = html.Div([
+    html.H1('Page 1'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'], 'LA', id='page-1-dropdown'),
+    html.Div(id='page-1-content'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/page-2'),
+    html.Br(),
+    dcc.Link('Go back to home', href='/'),
+])
+
+@callback(Output('page-1-content', 'children'),
+              [Input('page-1-dropdown', 'value')])
+def page_1_dropdown(value):
+    return f'You have selected {value}'
+
+
+page_2_layout = html.Div([
+    html.H1('Page 2'),
+    dcc.RadioItems(['Orange', 'Blue', 'Red'], 'Orange', id='page-2-radios'),
+    html.Div(id='page-2-content'),
+    html.Br(),
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    dcc.Link('Go back to home', href='/')
+])
+
+@callback(Output('page-2-content', 'children'),
+              [Input('page-2-radios', 'value')])
+def page_2_radios(value):
+    return f'You have selected {value}'
+
+
+# Update the index
+@callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/page-1':
+        return page_1_layout
+    elif pathname == '/page-2':
+        return page_2_layout
+    else:
+        return index_page
+    # You could also return a 404 "URL not found" page here
 
 @server.route("/dash")
 def my_dash_app():
