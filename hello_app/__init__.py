90d2379194b1ea20b23b23e9e4c441dc93a4244a
agaitskellwork <100278937+agaitskellwork@users.noreply.github.com>
2022-03-03 13:32:07 +0000

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -14,9 +14,9 @@ app = dash.Dash(__name__, server=server,  url_base_pathname='/dash/')
 #app.layout = html.Div(id='dash-container')
 app.layout = html.Div([html.H1('Simple Layout',style={'textAlign':'center'})])
 
-@server.route("/")
-def home():
-    return render_template("home.html")
+@server.route('/hello')
+def hello():
+    return 'Hello, World!'
 
 @server.route("/dash")
 def my_dash_app():
