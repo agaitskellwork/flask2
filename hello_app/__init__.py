5f8d86982c0c46aa8fee9c238a5fdec2b2b2e683
agaitskellwork <100278937+agaitskellwork@users.noreply.github.com>
2022-03-03 14:39:23 +0000

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -11,6 +11,10 @@ from flask import render_template
 server = flask.Flask(__name__, template_folder="templates")
 app = dash.Dash(__name__, server=server,  url_base_pathname='/dash/')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 #app.layout = html.Div(id='dash-container')
 app.layout = html.Div([html.H1('Simple Layout',style={'textAlign':'center'})])
 
